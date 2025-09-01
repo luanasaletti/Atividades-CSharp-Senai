03feb0b1d16c99d3640b206ecf654777d9f4ddee
BrunoAlmeidaRamos <robrumi01@gmail.com>
2025-09-01 11:58:46 -0300

    Atividade POO

---
+++ AtividadePOO/AtividadePOO/Pessoa.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadePOO
+{
+    public class Pessoa
+    {
+        public string nome;
+        public int idade;
+
+        public void SetNome(string valor)
+        {
+            nome = valor;
+        }
+
+        public string GetNome()
+        {
+            return nome;
+        }
+
+        public void SetIdade(int valor)
+        {
+            if (valor > 0)
+            {
+                idade = valor;
+            }
+            else
+            {
+                Console.WriteLine("Idade invalida, insira uma idade maior que 0");
+            }
+        }
+
+        public int GetIdade()
+        {
+            return idade;
+        }
+
+        public void ExibirDados()
+        {
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Idade: " + idade);
+        }
+    }
+}
---
+++ AtividadePOO/AtividadePOO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using AtividadePOO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa pessoa1 = new Pessoa();
+
+        // Entrada de dados pelo usuário
+        Console.Write("Digite o nome: ");
+        string nome = Console.ReadLine();
+        pessoa1.SetNome(nome);
+
+        Console.Write("Digite a idade: ");
+        int idade = int.Parse(Console.ReadLine());
+        pessoa1.SetIdade(idade); // Aqui já faz a validação
+
+        Console.WriteLine();
+        pessoa1.ExibirDados();
+
+        Console.ReadLine();
+    }
+}
